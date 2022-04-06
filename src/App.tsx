e7cbad4a16ac53fcaa1ab25c1c5818ddab031216
Eduardo <drdpedroso@gmail.com>
2022-04-05 22:32:48 -0300

    another try

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './App.css'
-import "leaflet/dist/leaflet.css";
+
 
 
 function App() {
   return (
         <MapContainer
-            style={{ height: "100vh" }}
             center={[0, 0]}
             maxZoom={7}
-            tms
-            zoom={0}>
+            style={{ width: '100%', height: '100%' }}
+            zoom={0}
+            minZoom={0}
+        >
           <TileLayer
               attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url={'../assets/{z}/{x}/{y}.png'}
