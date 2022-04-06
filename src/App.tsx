d104c72baa3107712243a9388a1343216ca8d8e1
Eduardo <drdpedroso@gmail.com>
2022-04-05 22:23:38 -0300

    Add leaflet css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './App.css'
+import "leaflet/dist/leaflet.css";
+
 
 function App() {
   return (
         <MapContainer
-            className="leaflet-container"
+            style={{ height: "100vh" }}
             center={[0, 0]}
             maxZoom={7}
+            tms
             zoom={0}>
-          <TileLayer url={'../assets/{z}/{x}/{y}.png'} />
+          <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={'../assets/{z}/{x}/{y}.png'}
+          />
         </MapContainer>
   );
 }
